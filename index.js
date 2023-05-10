ef1831fc03cf874d6f9abb0101c68c2d151cf6d6
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-05-10 13:45:47 +0200

    Added posibilty to upload files using fileUpload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import app from "./app.js";
 import { getParamsFromUrl } from "./utils/utils.js";
-
 import {
   connectServer,
   getAllBikesWhere,
@@ -10,9 +9,45 @@ import {
   updateBike,
   getAllBikesWithPagination,
 } from "./database/connect.js";
+import * as path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fileUpload from "express-fileupload";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(
+  fileUpload({
+    limits: {
+      fileSize: 10000000,
+    },
+    abortOnLimit: true,
+  })
+);
 
 const PORT = 3001;
 let connection;
+
+// Uploading files http://localhost:3001
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("/upload", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  const file = req.files.myFile;
+
+  const path = __dirname + "/public/bikesImages" + file.name;
+
+  file.mv(path, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send({ status: "success", path: path });
+  });
+});
 
 //GET - Pagination for example http://localhost:3001/bikes?limit=4&page=2
 app.get("/bikes", async (req, res) => {
