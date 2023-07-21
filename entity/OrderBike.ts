f87f5d1340e061f9d2bfb4fc844248da6f2b91ae
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-07-21 16:01:33 +0200

    Fixed DELETE issue

--- entity/OrderBike.ts
+++ entity/OrderBike.ts
@@ -16,7 +16,7 @@ export class OrderBike {
   @Column()
   quantity!: number;
 
-  @ManyToOne(() => Orders, (order) => order.orderBikes)
+  @ManyToOne(() => Orders, (order) => order.orderBikes, { onDelete: "CASCADE" })
   @JoinColumn({ name: "order_id" })
   order: Orders | undefined;
 
